ce6cd03334d8a7162bc682b066266b958f3a91a0
Seongmin <lsm@buptle.com>
2026-05-13 14:59:06 +0900

    리액트 컴파일러 eslint

--- eslint.config.js
+++ eslint.config.js
@@ -5,9 +5,10 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
 import react from 'eslint-plugin-react';
 import prettierConfig from 'eslint-config-prettier';
+import reactCompiler from 'eslint-plugin-react-compiler';
 
 export default tseslint.config(
-    { ignores: ['dist', 'node_modules', '.next', 'out', 'build'] },
+    { ignores: ['dist', 'node_modules', '.next', 'out', 'build', 'e2e', 'playwright-report', 'test-results', 'playwright.config.ts'] },
     {
       extends: [
         js.configs.recommended,
@@ -29,10 +30,12 @@ export default tseslint.config(
         react,
         'react-hooks': reactHooks,
         'react-refresh': reactRefresh,
+        'react-compiler': reactCompiler,
       },
       rules: {
         ...reactHooks.configs.recommended.rules,
         'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+        'react-compiler/react-compiler': 'error',
 
         // [사용자 요청 규칙]
         'react-hooks/static-components': 'off', // (참고: 이 규칙은 공식 플러그인에는 없지만 요청대로 유지)
